a3ac3b8044b6e0946921bfc174be01b63337c1a5
Allison Sadin <aesadin@gmail.com>
2020-07-20 13:26:10 -0700

    added form labels as well as the properties for variables
    Co-authored-by: MarielHamson <mariel.hamson@gmail.com>

---
+++ MadLib/Models/MadLibsVariables.cs
@@ -0,0 +1,14 @@
+namespace MadLib.Models
+{
+  public class MadLibVariables
+  {
+    public string CollectiveNoun { get; set; }
+    public string Thing { get; set; }
+    public string KitchenUtensils { get; set; }
+    public string Name { get; set; }
+    public string Shape { get; set; }
+    public string ClothingItem { get; set; }
+    public string Color { get; set; }
+    public string SeaCreature { get; set; }
+  }
+}
